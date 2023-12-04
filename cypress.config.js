f213c05d1f3977f00af7f2dcd998377a65411cbb
Tomas Augusto <tomasdl684@gmail.com>
2023-12-04 16:46:49 -0300

    config page load

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   watchForFileChanges: false,
   defaultCommandTimeout: 5000,
+  pageLoadTimeout: 7500,
   viewportWidth: 1920,
   viewportHeight: 1080,
   retries: {
